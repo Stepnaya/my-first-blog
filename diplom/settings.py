cdce05529cc3d85601ad89fa85edc90ff10c212f
Stepnaya <ssnicker@yandex.ru>
2016-03-21 19:53:28 +0300

    Update settings.py

--- diplom/settings.py
+++ diplom/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'diplom.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': diplom.db),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3),
     }
 }
 
